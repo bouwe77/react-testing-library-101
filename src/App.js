91141dea7f180286018f8bbbda8a76e7ae6b54bf
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-04-28 08:51:28 +0200

    Switched from a counter to an adventure game

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 function App() {
   return (
     <>
-      <h1>My Counter App</h1>
-      The counter is 0
+      <h1>My Adventure Game</h1>
+
+      <p>
+        You are a brave knight who has been given a quest by the king. You must find the princess and save her from the
+        evil dragon.
+      </p>
+
+      <p>
+        You are riding through the fields. In the distance, to the north, a castle appears. To the east is the forest.
+      </p>
+
       <div>
-        <button>Increment</button>
+        <Button>Go north</Button>
+        <Button>Go east</Button>
       </div>
     </>
+  )
+}
+
+function Button({ children, ...rest }) {
+  return (
+    <button style={{ margin: '10px 20px 0px 0px' }} {...rest}>
+      {children}
+    </button>
   )
 }
 
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,14 +1,21 @@
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('My Adventure Game', () => {
   render(<App />)
-  screen.getByText('My Counter App')
 
-  screen.getByText('The counter is 0')
+  screen.getByText('My Adventure Game')
 
-  const incrementButton = screen.getByRole('button', { name: 'Increment' })
-  incrementButton.click()
+  screen.getByText(
+    'You are a brave knight who has been given a quest by the king. You must find the princess and save her from the evil dragon.',
+  )
 
-  screen.getByText('The counter is 1')
+  screen.getByText(
+    'You are riding through the fields. In the distance, to the north, a castle appears. To the east is the forest.',
+  )
+
+  // screen.getByText('There is a castle in the distance, to the north. To the east is the forest.')
+
+  screen.getByRole('button', { name: 'Go north' })
+  screen.getByRole('button', { name: 'Go east' })
 })
