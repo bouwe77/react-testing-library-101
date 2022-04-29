7334d273f293708f9ac09e0404a775db2f2989da
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2022-04-29 07:25:14 +0200

    More meaningful names for directions, instead of north, south, etc.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
+import { useState } from 'react'
+
+const story = {
+  field: {
+    texts: [
+      'You are on your horse, in the fields, staring into the distance. There, you can see a castle.',
+      'On your right is the forest.',
+    ],
+    directions: { 'Go to the castle': 'castle', 'Head into the forest': 'forest' },
+  },
+  castle: {
+    texts: ['You arrive at the castle.'],
+    directions: { 'Back to the fields': 'field', 'Enter the castle': 'castle' },
+  },
+  forest: { texts: ['You arrive at the forest.'], directions: { 'Leave the forest': 'field' } },
+}
+
 function App() {
+  const [location, setLocation] = useState('field')
+  const [gameStarted, setGameStarted] = useState(false)
+
+  const goTo = (newLocation) => {
+    setLocation(newLocation)
+    if (!gameStarted) setGameStarted(true)
+  }
+
   return (
     <>
       <h1>My Adventure Game</h1>
 
-      <p>
-        You are a brave knight who has been given a quest by the king. You must find the princess and save her from the
-        evil dragon.
-      </p>
+      {/* TODO Deze tekst alleen bij de eerste render, dus een mooie use case om later een NOT in the document te doen... */}
+      {!gameStarted ? (
+        <p>
+          You are a brave knight who has been given a quest by the king. You must find the princess and save her from
+          the evil dragon. You are riding through the fields.
+        </p>
+      ) : null}
 
-      <p>
-        You are riding through the fields. In the distance, to the north, a castle appears. To the east is the forest.
-      </p>
+      {story[location].texts.map((line, index) => (
+        <p key={index}>{line}</p>
+      ))}
 
       <div>
-        <Button>Go north</Button>
-        <Button>Go east</Button>
+        {Object.keys(story[location].directions).map((direction) => (
+          <Button key={direction} onClick={() => goTo(story[location].directions[direction])}>
+            {direction}
+          </Button>
+        ))}
       </div>
     </>
   )
--- src/App.spec.js
+++ src/App.spec.js
@@ -1,21 +1,39 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import App from './App'
 
-test('My Adventure Game', () => {
+test('My Adventure Game', async () => {
   render(<App />)
 
   screen.getByText('My Adventure Game')
 
   screen.getByText(
-    'You are a brave knight who has been given a quest by the king. You must find the princess and save her from the evil dragon.',
+    'You are a brave knight who has been given a quest by the king. You must find the princess and save her from the evil dragon. You are riding through the fields.',
   )
 
-  screen.getByText(
-    'You are riding through the fields. In the distance, to the north, a castle appears. To the east is the forest.',
-  )
+  screen.getByText('You are on your horse, in the fields, staring into the distance. There, you can see a castle.')
+  screen.getByText('On your right is the forest.')
 
-  // screen.getByText('There is a castle in the distance, to the north. To the east is the forest.')
+  // I just wanna know the "Head into the forest" button's there
+  screen.getByRole('button', { name: 'Head into the forest' })
 
-  screen.getByRole('button', { name: 'Go north' })
-  screen.getByRole('button', { name: 'Go east' })
+  // I wanna know the "Go to the castle" button's there, but I also assign it, so I can click it
+  const toCastle = screen.getByRole('button', { name: 'Go to the castle' })
+  await userEvent.click(toCastle)
+
+  // Normally you have to wait for a state update, but because we've already waited for the click, we can just check the text
+  screen.getByText('You arrive at the castle.')
+
+  // Now that we started the game by clicking our first button the introductory text should be gone
+  expect(
+    screen.queryByText(
+      'You are a brave knight who has been given a quest by the king. You must find the princess and save her from the evil dragon. You are riding through the fields.',
+    ),
+  ).not.toBeInTheDocument()
+
+  // A "Back to the fields" button should be there
+  screen.getByRole('button', { name: 'Back to the fields' })
+
+  // There should also be a "Enter the castle" button
+  screen.getByRole('button', { name: 'Enter the castle' })
 })
